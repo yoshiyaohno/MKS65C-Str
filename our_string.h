262e5305a9cb3de2b4ccae4c9e77332551d7380e
Yoshiya <yohno@stuy.edu>
2018-09-27 11:36:15 -0400

    Add header file

---
+++ our_string.h
@@ -0,0 +1,24 @@
+
+//Yoshi
+int our_strlen( char * );
+
+// char * our_strcpy( char *dest, char *source );
+
+//Mandy
+char * our_strncpy( char *dest, char *source, int n);
+
+
+//Yoshi
+char * our_strcat( char *dest, char *source );
+
+// char * our_strncat( char *dest, char *source, int n);
+
+//Mandy
+int our_strcmp( char *s1, char *s2 );
+
+//Mandy
+char * our_strchr( char *s, char c );
+
+
+//Yoshi
+char * our_strstr( char *haystack, char * needle );
