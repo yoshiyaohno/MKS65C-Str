8352513fe72222db62058ab3ee68c2f6d6db42d9
Yoshiya <yohno@stuy.edu>
2018-09-27 19:41:41 -0400

    Add boilerplate code

--- our_string.c
+++ our_string.c
@@ -2,28 +2,49 @@
 #include "our_string.h"
 
 
-// read directly from header
-////////////
-// //Yoshi
-// int our_strlen( char * );
-// 
-// // char * our_strcpy( char *dest, char *source );
-// 
-// //Mandy
-// char * our_strncpy( char *dest, char *source, int n);
-// 
-// 
-// //Yoshi
-// char * our_strcat( char *dest, char *source );
-// 
-// // char * our_strncat( char *dest, char *source, int n);
-// 
-// //Mandy
-// int our_strcmp( char *s1, char *s2 );
-// 
-// //Mandy
-// char * our_strchr( char *s, char c );
-// 
-// 
-// //Yoshi, bonus
-// char * our_strstr( char *haystack, char * needle );
+/* Yoshi */
+int our_strlen( char * )
+{
+    // code here
+    return 0;
+}
+
+// char * our_strcpy( char *dest, char *source ) { } 
+
+/* Mandy */
+char * our_strncpy( char *dest, char *source, int n)
+{
+    // code here
+    return (char*) 0;
+}
+
+
+/* Yoshi */
+char * our_strcat( char *dest, char *source )
+{
+    // code here
+    return (char*) 0;
+}
+
+// char * our_strncat( char *dest, char *source, int n);
+
+/* Mandy */
+int our_strcmp( char *s1, char *s2 )
+{
+    // code here
+    return 0;
+}
+
+/* Mandy */
+char * our_strchr( char *s, char c )
+{
+    // code here
+    return (char*) 0;
+}
+
+/* Yoshi (bonus) */
+char * our_strstr( char *haystack, char * needle )
+{
+    // code here
+    return (char*) 0;
+}
