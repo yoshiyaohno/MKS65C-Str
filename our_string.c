67cd31060522ad24ae978993b0093bad82563d2d
Yoshiya <yohno@stuy.edu>
2018-09-27 19:10:05 -0400

    Add barebones source files

---
+++ our_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "our_string.h"
+
+
+// read directly from header
+////////////
+// //Yoshi
+// int our_strlen( char * );
+// 
+// // char * our_strcpy( char *dest, char *source );
+// 
+// //Mandy
+// char * our_strncpy( char *dest, char *source, int n);
+// 
+// 
+// //Yoshi
+// char * our_strcat( char *dest, char *source );
+// 
+// // char * our_strncat( char *dest, char *source, int n);
+// 
+// //Mandy
+// int our_strcmp( char *s1, char *s2 );
+// 
+// //Mandy
+// char * our_strchr( char *s, char c );
+// 
+// 
+// //Yoshi, bonus
+// char * our_strstr( char *haystack, char * needle );
---
+++ tester.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "our_string.h"
+
+int main()
+{
+    printf("bep\n");
+    return 0;
+}
