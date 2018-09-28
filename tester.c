bbbbe3f07aa523212bc6fecb2b00bcd3605d0f6b
Yoshiya <yohno@stuy.edu>
2018-09-28 14:56:49 -0400

    Add strcat() testing

--- tester.c
+++ tester.c
@@ -10,6 +10,14 @@ int main()
     printf("Length of \"oof\"\n");
     printf("\tus: %d\n\tlib: %lu\n", our_strlen("oof"), strlen("oof"));
 
+    /* strcat() testing */
+    char oof1[30] = "oofouch";
+    char oof2[30] = "oofouch";
+    printf("\nTesting strcat:\nstrcat oofouch owie:\n");
+    printf("\tus: %s\n\tlib: %s\n",
+            our_strcat(oof1, "owie"),
+            strcat(oof2, "owie") );
+
     /* strstr() testing */
     // general test
     printf("\nTesting strstr:\nstrstr stroofing oof:\n");
