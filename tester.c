be0d293bbfcf573007c8dfa459209a69fb00ce9f
Yoshiya <yohno@stuy.edu>
2018-09-28 12:27:42 -0400

    Add additional strstr() edge cases

--- tester.c
+++ tester.c
@@ -4,16 +4,30 @@
 
 int main()
 {
+    /* strlen testing */
     printf("\nTesting strlen:\nLength of \"\"\n");
     printf("\tus: %d\n\tlib: %lu\n", our_strlen(""), strlen(""));
     printf("Length of \"oof\"\n");
     printf("\tus: %d\n\tlib: %lu\n", our_strlen("oof"), strlen("oof"));
 
+    /* strstr() testing */
+    // general test
     printf("\nTesting strstr:\nstrstr stroofing oof:\n");
     printf("\tus: %p\n\tlib: %p\n",
             our_strstr("stroofing", "oof"),
             strstr("stroofing", "oof") );
+    // test word at beginning, multiple occurences
+    printf("strstr thinkthin thin:\n");
+    printf("\tus: %p\n\tlib: %p\n",
+            our_strstr("thinkthin", "thin"),
+            strstr("thinkthin", "thin") );
+    // test word at end
+    printf("strstr suet-baguette uette:\n");
+    printf("\tus: %p\n\tlib: %p\n",
+            our_strstr("suet-baguette", "uette"),
+            strstr("suet-baguette", "uette") );
     printf("strstr oofouchow howie:\n");
+    // test no word, hitting end of haystack
     printf("\tus: %p\n\tlib: %p\n",
             our_strstr("oofouchow", "howie"),
             strstr("oofouchow", "howie") );
